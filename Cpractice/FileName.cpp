9c2275913ea53719fd92f6d54bb311e604f7ae7b
choigh316 <choigh666666@naver.com>
2023-09-07 10:50:30 +0900

    good

--- Cpractice/FileName.cpp
+++ Cpractice/FileName.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
-	printf("hell world!");
+	int N;
+	printf("몇 층 피라미드를 원해?\n대답.\n");
+	scanf_s("%d", &N);
+	for (int i = 1; i <= N; i++)
+	{
+		for (int j = i; j <= N - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 1; k <= 2 * i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
 }
