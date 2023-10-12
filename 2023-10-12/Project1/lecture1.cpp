8e9156b7f0c51fdcf1c1499cec41c50db62cda54
vphatree12 <144293453+choigh316@users.noreply.github.com>
2023-10-12 11:21:36 +0900

    Update lecture1.cpp

--- 2023-10-12/Project1/lecture1.cpp
+++ 2023-10-12/Project1/lecture1.cpp
@@ -2,5 +2,14 @@
 
 int main()
 {
-	printf("good");
+	int a = 3;
+	int* ptr1 = &a;
+
+	char b = "b";
+	int* ptr2 = &b;
+	printf("%d\n", ptr1);
+	printf("%d\n", sizeof(ptr1));
+
+	printf("%d\n", ptr2);
+	printf("%d\n", sizeof(ptr2));
 }
